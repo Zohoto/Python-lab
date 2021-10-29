77e5dd3942a736820b9640cc2d1b0d2b0cbd7a23
Zohoto <andronik-03@mail.ru>
2021-10-29 21:39:02 +0500

    Fixed code

--- lab5/lab5_4.4.py
+++ lab5/lab5_4.4.py
@@ -1,15 +1,16 @@
 def money(coin):
     rub = coin // 100
     coin = coin % 100
-    if rub == 1:
-        print(str(rub) + " РУБЛЬ")
-    elif (20 >= rub >= 10) or (rub % 10 == "0", "5", "6", "7", "8", "9"):
-        print(str(rub) + " РУБЛЕЙ")
-    else:
-        print(str(rub) + " РУБЛЯ")
-    if coin == 1:
+    if rub > 0:
+        if rub % 10 == 1:
+            print(str(rub) + " РУБЛЬ")
+        elif (20 >= rub >= 10) or (rub % 10 == "0", "5", "6", "7", "8", "9"):
+            print(str(rub) + " РУБЛЕЙ")
+        else:
+            print(str(rub) + " РУБЛЯ")
+    if coin % 10 == 1:
         print(str(coin) + " КОПЕЙКА")
-    elif 20 >= coin >= 10 or (coin % 10 == "0", "5", "6", "7", "8", "9"):
+    elif (20 >= coin >= 10) or (coin % 10 == "0", "5", "6", "7", "8", "9"):
         print(str(coin) + " КОПЕЕК")
     else:
         print(str(coin) + " КОПЕЙКИ")
