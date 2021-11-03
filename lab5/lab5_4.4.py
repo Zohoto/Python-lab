02b148c235cee040c9026b3f48f3ffb68c2e1068
Zohoto <andronik-03@mail.ru>
2021-11-04 00:31:08 +0500

    Bug in print rub, coin

--- lab5/lab5_4.4.py
+++ lab5/lab5_4.4.py
@@ -4,17 +4,18 @@ def money(coin):
     if rub > 0:
         if rub % 10 == 1:
             print(str(rub) + " РУБЛЬ")
-        elif (20 >= rub >= 10) or (rub % 10 == "0", "5", "6", "7", "8", "9"):
+        elif (10 <= rub <= 20) or (5 <= rub % 10 <= 9) or rub % 10 == 0:
             print(str(rub) + " РУБЛЕЙ")
         else:
             print(str(rub) + " РУБЛЯ")
-    if coin % 10 == 1:
-        print(str(coin) + " КОПЕЙКА")
-    elif (20 >= coin >= 10) or (coin % 10 == "0", "5", "6", "7", "8", "9"):
-        print(str(coin) + " КОПЕЕК")
-    else:
-        print(str(coin) + " КОПЕЙКИ")
+    if coin > 0:
+        if coin % 10 == 1:
+            print(str(coin) + " КОПЕЙКА")
+        elif (10 <= rub <= 20) or (5 <= rub % 10 <= 9) or rub % 10 == 0:
+            print(str(coin) + " КОПЕЕК")
+        else:
+            print(str(coin) + " КОПЕЙКИ")
 
 
-coin = int(input("Введите колчиество копеек: "))
+coin = int(input("Введите количество копеек: "))
 money(coin)
